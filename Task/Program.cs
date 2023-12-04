abd731fb97d92a3e7a63d9245698373ed8341b9a
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2023-12-04 22:32:06 +0200

    Uncomment some path of code

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,43 +4,43 @@ class Program
     static void Main()
     {
         int N=5, M=5;
-        //bool check0 = false, check1, check2, check3, check4, check5 = false;
-        //Console.WriteLine("Enter matrix size:");
-        //do
-        //{
-        //    check1 = false; check2 = false;
-        //    Console.Write("N = ");
-        //    do
-        //    {
-        //        check1 = int.TryParse(Console.ReadLine(), out N);
-        //    } while (check1 == false);
-        //    Console.Write("M = ");
-        //    do
-        //    {
-        //        check2 = int.TryParse(Console.ReadLine(), out M);
-        //    } while (check2 == false);
-        //    if (N > 0 && M > 0) check0 = true;
-        //    else Console.WriteLine("N and M must be greater than 0 to create matrix");
-        //} while (check0 == false);
+        bool check0 = false, check1, check2, check3, check4, check5 = false;
+        Console.WriteLine("Enter matrix size:");
+        do
+        {
+            check1 = false; check2 = false;
+            Console.Write("N = ");
+            do
+            {
+                check1 = int.TryParse(Console.ReadLine(), out N);
+            } while (check1 == false);
+            Console.Write("M = ");
+            do
+            {
+                check2 = int.TryParse(Console.ReadLine(), out M);
+            } while (check2 == false);
+            if (N > 0 && M > 0) check0 = true;
+            else Console.WriteLine("N and M must be greater than 0 to create matrix");
+        } while (check0 == false);
         int[,] array = new int[N, M];
         int min = -10, max=10;
-        //Console.Write("Enter the range of number generation\n");
-        //do
-        //{
-        //    check3 = false; check4 = false;
-        //    Console.Write("Minimum generated value: ");
-        //    do
-        //    {
-        //        check3 = int.TryParse(Console.ReadLine(), out min);
-        //    } while (check3 == false);
-        //    Console.Write("Maximum generated value: ");
-        //    do
-        //    {
-        //        check4 = int.TryParse(Console.ReadLine(), out max);
-        //    } while (check4 == false);
-        //    if (min < max) check5 = true;
-        //    else Console.WriteLine("Minimum value must be greater than maximum! Try again.");
-        //} while (check5 == false);
+        Console.Write("Enter the range of number generation\n");
+        do
+        {
+            check3 = false; check4 = false;
+            Console.Write("Minimum generated value: ");
+            do
+            {
+                check3 = int.TryParse(Console.ReadLine(), out min);
+            } while (check3 == false);
+            Console.Write("Maximum generated value: ");
+            do
+            {
+                check4 = int.TryParse(Console.ReadLine(), out max);
+            } while (check4 == false);
+            if (min < max) check5 = true;
+            else Console.WriteLine("Minimum value must be greater than maximum! Try again.");
+        } while (check5 == false);
         array = arrayGen(array, min, max);
         printArray(array);
         positiveElementsCounter(array);
